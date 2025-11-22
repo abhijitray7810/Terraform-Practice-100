9048e06b10a6206c65d9213f5566c8ec5e9c15ed
Abhijit RAY <royabhijits928@gmail.com>
2025-11-22 15:21:06 +0530

    Create main.tf

---
+++ Day 02: Create Security Group Using Terraform/main.tf
@@ -0,0 +1,46 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Create the security group
+resource "aws_security_group" "nautilus_sg" {
+  name        = "nautilus-sg"
+  description = "Security group for Nautilus App Servers"
+  vpc_id      = data.aws_vpc.default.id
+
+  # HTTP inbound rule
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP access"
+  }
+
+  # SSH inbound rule
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nautilus-sg"
+  }
+}
