a2f6e8ffcc99c5695c2021bc566189ce20f992bc
Abhijit RAY <royabhijits928@gmail.com>
2025-12-15 22:18:28 +0530

    Create main.tf

---
+++ Day 10: Create Snapshot Using Terraform/main.tf
@@ -0,0 +1,35 @@
+# Data source to fetch the existing volume
+data "aws_ebs_volume" "nautilus_vol" {
+  most_recent = true
+
+  filter {
+    name   = "tag:Name"
+    values = ["nautilus-vol"]
+  }
+}
+
+# Create the EBS snapshot
+resource "aws_ebs_snapshot" "nautilus_snapshot" {
+  volume_id   = data.aws_ebs_volume.nautilus_vol.id
+  description = "Nautilus Snapshot"
+
+  tags = {
+    Name = "nautilus-vol-ss"
+  }
+}
+
+# Output to verify the snapshot status
+output "snapshot_id" {
+  value       = aws_ebs_snapshot.nautilus_snapshot.id
+  description = "The ID of the created snapshot"
+}
+
+output "snapshot_status" {
+  value       = aws_ebs_snapshot.nautilus_snapshot.state
+  description = "The status of the snapshot"
+}
+
+output "volume_id" {
+  value       = data.aws_ebs_volume.nautilus_vol.id
+  description = "The ID of the source volume"
+}
