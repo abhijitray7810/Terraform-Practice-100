70d49161957fe7dc20e76a70e7f9fc6d03d3200b
Abhijit RAY <royabhijits928@gmail.com>
2026-03-27 17:17:06 +0530

    Update main.tf

--- Day 44: Create VPC and Subnet Using Terraform/main.tf
+++ Day 44: Create VPC and Subnet Using Terraform/main.tf
@@ -1,1 +1,22 @@
-q
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "datacenter_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = var.KKE_VPC_NAME
+  }
+}
+
+resource "aws_subnet" "datacenter_subnet" {
+  vpc_id     = aws_vpc.datacenter_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = var.KKE_SUBNET_NAME
+  }
+
+  depends_on = [aws_vpc.datacenter_vpc]
+}
